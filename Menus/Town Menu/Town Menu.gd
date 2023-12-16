22f37800bf761224ee6784bda9c0053bc4d884d9
Mary Piering <maryilewis@gmail.com>
2023-12-16 17:06:27 -0500

    working on menus

--- Menus/Town Menu/Town Menu.gd
+++ Menus/Town Menu/Town Menu.gd
@@ -1,6 +1,6 @@
-extends Node2D
+extends Control
 
-#TODO: I think this should be a tooltip
+#TODO: I think this should be a log and a tooltip
 
 var town: TownNode
 
@@ -10,7 +10,10 @@ func _ready():
 
 func show_menu(_town):
 	town = _town
-	$RichTextLabel.text = "You have arrived in " + town.display_name
+	#$PanelContainer/Panel/MarginContainer/RichTextLabel.text = "You have arrived in " + town.display_name
+	#add_button ( String text, bool right=false, String action="" )
+	#for job in list, is it this town and do you have the cargo? if so, add a button (hide when the job is complete)
+	#for export in this town, add a button to pick some up (disabled if you have no room)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
