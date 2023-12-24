de47926d9f6cb2ce49a6189d8064793811f1972c
Mary Piering <maryilewis@gmail.com>
2023-12-24 12:02:31 -0500

    sheep, map from objects

--- Autoloads/game_data.gd
+++ Autoloads/game_data.gd
@@ -9,8 +9,7 @@ var train_camera: TrainCamera
 #endregion
 
 #region rng
-# TODO move to util
-var rng = RandomNumberGenerator.new()
+var rng = RandomNumberGenerator.new() # TODO move to util
 #endregion
 
 #region map
@@ -18,6 +17,7 @@ signal map_updated
 
 @onready var flat = preload("res://Square Scenes/flat/flat.tscn")
 @onready var wheat = preload("res://Square Scenes/wheat/wheat.tscn")
+@onready var sheep = preload("res://Square Scenes/sheep/sheep.tscn")
 @onready var forest = preload("res://Square Scenes/forest/forest.tscn")
 @onready var water = preload("res://Square Scenes/water/water.tscn")
 @onready var town = preload("res://Square Scenes/town/town.tscn")
@@ -25,6 +25,21 @@ var map_plan = []
 var map_nodes = []
 var map_size = 24
 var towns = []
+
+enum SquareType {
+	FLAT = 1,
+	TOWN = 2,
+	WATER = 3,
+	FOREST = 4,
+	WHEAT = 5,
+	SHEEP = 6
+}
+
+class SquareDetails:
+	var display_name: String
+	var square_type: SquareType
+	
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -41,59 +56,70 @@ func init_map_data2(size):
 	for i in size:
 		for j in size:
 			var dice_roll = rng.randi_range(1, 100)
+			print("i: ", i, ", j: ", j, ", roll: ", dice_roll)
 			var node_above
 			var node_left
 			if (j > 0):
 				node_left = map_plan[i][j-1]
 			if (i > 0):
 				node_above = map_plan[i-1][j]
-			if ((node_left == 'w' or node_above == 'w') and dice_roll >= 50):
-				map_plan[i].append('w')
-			elif ((node_left == 'f' or node_above == 'f') and dice_roll >= 70):
-				map_plan[i].append('f')
+			if ((node_left && node_left.key == SquareType.WATER or node_above && node_above.key == SquareType.WATER) and dice_roll >= 50):
+				map_plan[i].append({
+					"key": SquareType.WATER
+				})
+			elif ((node_left && node_left.key == SquareType.FOREST or node_above && node_above.key == SquareType.FOREST) and dice_roll >= 70):
+				map_plan[i].append({
+					"key": SquareType.FOREST
+				})
 			elif (dice_roll >= 90):
-				map_plan[i].append('w')
+				map_plan[i].append({
+					"key": SquareType.WATER
+				})
 			elif (dice_roll >= 80):
-				map_plan[i].append('f')
+				map_plan[i].append({
+					"key": SquareType.FOREST
+				})
 			elif (dice_roll >= 75):
-				map_plan[i].append('t')
+				map_plan[i].append({
+					"key": SquareType.TOWN
+				})
 			elif (dice_roll >= 70):
-				map_plan[i].append('wh')
+				map_plan[i].append({
+					"key": SquareType.WHEAT
+				})
+			elif (dice_roll >= 65):
+				map_plan[i].append({
+					"key": SquareType.SHEEP
+				})
 			else:
-				map_plan[i].append('.')
-	map_plan[size/2][size/2] = 't'
-
-func init_map_data():
-	map_plan.append(['.', '.', '.', '.', 'w', '.', '.', '.', '.', '.'])
-	map_plan.append(['.', '.', '.', '.', 'w', 'w', '.', '.', '.', '.'])
-	map_plan.append(['.', '.', 'r', 'r', 'r', 'w', 'w', '.', '.', '.'])
-	map_plan.append(['.', '.', 'r', 'f', 'r', 'r', 'r', 'r', 'r', '.'])
-	map_plan.append(['.', '.', 'r', 'f', 'f', 'f', 'w', '.', 'r', '.'])
-	map_plan.append(['.', '.', '.', '.', 'f', '.', 'w', '.', 'r', '.'])
-	map_plan.append(['.', 't', 'r', 'r', 'r', 'r', 'r', 'r', 't', '.'])
-	map_plan.append(['.', '.', '.', '.', '.', 'f', 'w', '.', '.', '.'])
-	map_plan.append(['w', 'w', '.', '.', '.', '.', 'w', 'w', 'f', '.'])
-	map_plan.append(['w', 'w', '.', '.', '.', '.', '.', 'w', '.', '.'])
+				map_plan[i].append({
+					"key": SquareType.FLAT
+				})
+	map_plan[size/2][size/2] = {
+		"key": SquareType.TOWN
+	}
 
 func create_map():
 	var size = GameData.map_plan.size()
 	for i in range(size):
 		GameData.map_nodes.append([])
 		for j in range(size): #assumes a square
-			var key = GameData.map_plan[i][j]
+			var key = GameData.map_plan[i][j].key
 			var new_square
 			match key:
-				'.':
+				SquareType.FLAT:
 					new_square = flat.instantiate()
-				'w':
+				SquareType.WATER:
 					new_square = water.instantiate()
-				'f':
+				SquareType.FOREST:
 					new_square = forest.instantiate()
-				't':
+				SquareType.TOWN:
 					new_square = town.instantiate()
 					towns.append(new_square)
-				'wh':
+				SquareType.WHEAT:
 					new_square = wheat.instantiate()
+				SquareType.SHEEP:
+					new_square = sheep.instantiate()
 			new_square.position = Vector3(i - size/2, 0, j - size/2)
 			new_square.map_x = i
 			new_square.map_y = j
---
+++ Square Scenes/sheep/sheep.gd
@@ -0,0 +1,15 @@
+class_name SheepNode extends SquareBaseNode
+
+func _init():
+	_is_buildable = true
+	_is_traversible = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
